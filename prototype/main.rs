5c98314c7b80d1a59444dad939675b3e8757e531
Alexander Fadeev <fadeevab.com@gmail.com>
2022-07-19 15:43:09 +0300

    Prototype pattern. (+Toml formatting.)

---
+++ prototype/main.rs
@@ -0,0 +1,17 @@
+#[derive(Clone)]
+struct Circle {
+    pub x: u32,
+    pub y: u32,
+    pub radius: u32,
+}
+
+fn main () {
+    let circle1 = Circle {x: 10, y: 15, radius: 10};
+
+    let mut circle2 = circle1.clone();
+    circle2.y = 125;
+    circle2.radius = 77;
+
+    println!("Circle #1: x = {}, y = {}, radius = {}", circle1.x, circle1.y, circle1.radius);
+    println!("Circle #2: x = {}, y = {}, radius = {}", circle2.x, circle2.y, circle2.radius);
+}
