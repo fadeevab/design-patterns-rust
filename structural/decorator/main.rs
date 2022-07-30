4661e2850971f2bab2a91094d9db8ba44a459c0e
Alexander Fadeev <fadeevab.com@gmail.com>
2022-07-30 12:25:11 +0300

    Decorator: a simple real-life I/O decorator

---
+++ structural/decorator/main.rs
@@ -0,0 +1,18 @@
+use std::io::{BufReader, Cursor, Read};
+
+fn main() {
+    let mut buf = [0u8; 10];
+
+    // A buffered reader decorates a vector reader which wraps input data.
+    let mut input = BufReader::new(Cursor::new("Input data"));
+
+    input.read(&mut buf).ok();
+
+    print!("Read from a buffered reader: ");
+
+    for byte in buf {
+        print!("{}", char::from(byte));
+    }
+
+    println!("");
+}
