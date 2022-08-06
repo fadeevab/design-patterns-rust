285155b7df01120a43af4820570af4080de6d425
Alexander Fadeev <fadeevab.com@gmail.com>
2022-08-06 22:55:28 +0300

    Template Method Pattern: conceptual example

---
+++ behavioral/template-method/README.md
@@ -0,0 +1,28 @@
+# Template Method
+
+Template Method is a default implementation of a trait method that defines a skeleton
+of an algorithm, and other methods can be redefined in concrete types.
+
+A referenced example: [Template Method in Python](https://refactoring.guru/design-patterns/template-method/python/example)
+
+```bash
+cargo run --bin template-method
+```
+
+Output:
+
+```
+Same client code can work with different concrete implementations:
+TemplateMethod says: I am doing the bulk of the work
+ConcreteStruct1 says: Implemented Operation1
+TemplateMethod says: But I let subclasses override some operations
+ConcreteStruct1 says: Implemented Operation2
+TemplateMethod says: But I am doing the bulk of the work anyway
+
+Same client code can work with different concrete implementations:
+TemplateMethod says: I am doing the bulk of the work
+ConcreteStruct1 says: Implemented Operation1
+TemplateMethod says: But I let subclasses override some operations
+ConcreteStruct1 says: Implemented Operation2
+TemplateMethod says: But I am doing the bulk of the work anyway
+```
---
+++ behavioral/template-method/main.rs
@@ -0,0 +1,66 @@
+trait TemplateMethod {
+    fn template_method(&self) {
+        self.base_operation1();
+        self.required_operations1();
+        self.base_operation2();
+        self.hook1();
+        self.required_operations2();
+        self.base_operation3();
+        self.hook2();
+    }
+
+    fn base_operation1(&self) {
+        println!("TemplateMethod says: I am doing the bulk of the work");
+    }
+
+    fn base_operation2(&self) {
+        println!("TemplateMethod says: But I let subclasses override some operations");
+    }
+
+    fn base_operation3(&self) {
+        println!("TemplateMethod says: But I am doing the bulk of the work anyway");
+    }
+
+    fn hook1(&self) {}
+    fn hook2(&self) {}
+
+    fn required_operations1(&self);
+    fn required_operations2(&self);
+}
+
+struct ConcreteStruct1;
+
+impl TemplateMethod for ConcreteStruct1 {
+    fn required_operations1(&self) {
+        println!("ConcreteStruct1 says: Implemented Operation1")
+    }
+
+    fn required_operations2(&self) {
+        println!("ConcreteStruct1 says: Implemented Operation2")
+    }
+}
+
+struct ConcreteStruct2;
+
+impl TemplateMethod for ConcreteStruct2 {
+    fn required_operations1(&self) {
+        println!("ConcreteStruct2 says: Implemented Operation1")
+    }
+
+    fn required_operations2(&self) {
+        println!("ConcreteStruct2 says: Implemented Operation2")
+    }
+}
+
+fn client_code(concrete: impl TemplateMethod) {
+    concrete.template_method()
+}
+
+fn main() {
+    println!("Same client code can work with different concrete implementations:");
+    client_code(ConcreteStruct1);
+    println!();
+
+    println!("Same client code can work with different concrete implementations:");
+    client_code(ConcreteStruct1);
+}
