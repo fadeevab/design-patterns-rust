55590518e6f87424808ed2fe06b89aa96104b890
Alexander Fadeev <fadeevab.com@gmail.com>
2022-08-11 01:35:50 +0300

    Iterator: improve documentation

--- behavioral/iterator/README.md
+++ behavioral/iterator/README.md
@@ -1,8 +1,12 @@
 # Iterator
 
-Iterator is a behavioral design pattern that allows sequential traversal through a complex data structure without exposing its internal details.
+**Iterator** is a behavioral design pattern that allows sequential traversal
+through a complex data structure without exposing its internal details.
 
 ## Standard Iterator
+
+Iterators are heavily used in idiomatic 🦀 Rust code. The first example is
+how to use iterators over a standard array collection.
 
 ```rust
 let array = &[1, 2, 3];
@@ -14,11 +18,25 @@ iterator.for_each(|e| print!("{}, ", e));
 
 ## Custom Iterator
 
+In Rust, the recommended way to define your *custom* iterator is to use a
+standard `Iterator` trait. The example doesn't contain a synthetic iterator
+interface, because it is really recommended to use the idiomatic Rust way.
+
 ```rust
 let users = UserCollection::new();
 let mut iterator = users.iter();
 
 iterator.next();
+```
+
+A `next` method is the only `Iterator` trait method which is mandatory to be
+implemented. It makes accessible a huge range of standard methods,
+e.g. `fold`, `map`, `for_each`.
+
+```rust
+impl Iterator for UserIterator<'_> {
+    fn next(&mut self) -> Option<Self::Item>;
+}
 ```
 
 ## Execution Result
--- behavioral/iterator/users.rs
+++ behavioral/iterator/users.rs
@@ -2,13 +2,19 @@ pub struct UserCollection {
     users: [&'static str; 3],
 }
 
+/// A custom collection contains an arbitrary user array under the hood.
 impl UserCollection {
+    /// Returns a custom user collection.
     pub fn new() -> Self {
         Self {
             users: ["Alice", "Bob", "Carl"],
         }
     }
 
+    /// Returns an iterator over a user collection.
+    ///
+    /// The method name may be different, however, `iter` is used as a de facto
+    /// standard in a Rust naming convention.
     pub fn iter(&self) -> UserIterator {
         UserIterator {
             index: 0,
@@ -17,17 +23,21 @@ impl UserCollection {
     }
 }
 
-/// UserIterator allows sequential traversal through a complex users collection
+/// UserIterator allows sequential traversal through a complex user collection
 /// without exposing its internal details.
 pub struct UserIterator<'a> {
     index: usize,
     user_collection: &'a UserCollection,
 }
 
-// `Iterator` is a standard interface for dealing with iterators from the Rust standard library.
+/// `Iterator` is a standard interface for dealing with iterators
+/// from the Rust standard library.
 impl Iterator for UserIterator<'_> {
     type Item = &'static str;
 
+    /// A `next` method is the only `Iterator` trait method which is mandatory to be
+    /// implemented. It makes accessible a huge range of standard methods,
+    /// e.g. `fold`, `map`, `for_each`.
     fn next(&mut self) -> Option<Self::Item> {
         if self.index < self.user_collection.users.len() {
             let user = Some(self.user_collection.users[self.index]);
