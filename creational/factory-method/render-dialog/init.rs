e0fdfb9c75fa2f15bc9b376355a605b6fc6f612a
Alexander Fadeev <fadeevab.com@gmail.com>
2022-08-29 13:25:24 +0300

    Fix clippy errors in the Factory Method example

--- creational/factory-method/render-dialog/init.rs
+++ creational/factory-method/render-dialog/init.rs
@@ -6,9 +6,9 @@ pub fn initialize() -> &'static dyn Dialog {
     // The dialog type is selected depending on the environment settings or configuration.
     if cfg!(windows) {
         println!("-- Windows detected, creating Windows GUI --");
-        return &WindowsDialog;
+        &WindowsDialog
     } else {
         println!("-- No OS detected, creating the HTML GUI --");
-        return &HtmlDialog;
-    };
+        &HtmlDialog
+    }
 }
