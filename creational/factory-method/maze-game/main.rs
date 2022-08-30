126c712e35af8900de19cc118ad94e8ade76918e
Alexander Fadeev <fadeevab.com@gmail.com>
2022-08-30 15:51:23 +0300

    Add explanatory comments to the MazeGame example

--- creational/factory-method/maze-game/main.rs
+++ creational/factory-method/maze-game/main.rs
@@ -5,10 +5,16 @@ mod ordinary_maze;
 use magic_maze::MagicMaze;
 use ordinary_maze::OrdinaryMaze;
 
+/// The game runs with different mazes depending on the concrete factory type:
+/// it's either an ordinary maze or a magic maze.
+///
+/// For demonstration purposes, both mazes are used to construct the game.
 fn main() {
-    let magic_maze = MagicMaze::new();
-    game::run(magic_maze);
-
+    // Option 1: The game starts with an ordinary maze.
     let ordinary_maze = OrdinaryMaze::new();
     game::run(ordinary_maze);
+
+    // Option 2: The game starts with a magic maze.
+    let magic_maze = MagicMaze::new();
+    game::run(magic_maze);
 }
